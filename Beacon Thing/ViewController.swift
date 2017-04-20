63f5c78c0b9a291ed2d68ac6723d9d04ac928bfb
Michael Harper <mharper@standalonecode.com>
2017-04-20 13:48:18 -0700

    Initial Commit

---
+++ Beacon Thing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Beacon Thing
+//
+//  Created by Michael Harper on 4/20/17.
+//  Copyright © 2017 Radius Networks, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
